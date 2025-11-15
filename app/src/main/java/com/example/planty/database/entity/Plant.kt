1eed0d0bdb433d910e471e1ce5730b3e1c4c5e3a
Dominik Dziadosz <domix98@interia.pl>
2025-11-15 11:51:36 +0100

    created some database functionality

---
+++ app/src/main/java/com/example/planty/database/entity/Plant.kt
@@ -0,0 +1,16 @@
+package com.example.planty.database.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Plant (
+    @PrimaryKey(autoGenerate = true)
+
+    val id: Int,
+    val name: String,
+    val description: String,
+    val photoUrl: String?,
+    val wateringFrequencyDays: Int,
+    val lastWatered: Long
+)
---
+++ app/src/main/java/com/example/planty/database/entity/WaterHistory.kt
@@ -0,0 +1,26 @@
+package com.example.planty.database.entity
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "watering_history",
+    foreignKeys = [
+        ForeignKey(
+            entity = Plant::class,
+            parentColumns = ["id"],
+            childColumns = ["plantId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
+
+data class WaterHistory (
+    @PrimaryKey(autoGenerate = true)
+
+    val id: Int = 0,
+    val plantId: Int,
+    val wateredDate: Long
+
+)
